bac8be1b66890f5a93866eff0bb842cc673dcba7
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-12 01:33:17 +0700

    Add files via upload

---
+++ PBOIF2-10116378-LATIHAN37-ProgramRataRataNilai/src/pboif2/pkg10116378/latihan37/programrataratanilai/Mahasiswa.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10116378.latihan37.programrataratanilai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class Mahasiswa {
+
+    public int nilaiMhs;
+        public double totalNilaiMhs;
+    Scanner scn = new Scanner(System.in);
+
+    public double HitungTotal(int parNilaiMhs, int parN){
+        for (int i=1; i<=parN; i++){
+            System.out.print("Nilai Mahasiswa ke-"+i+" : ");
+            parNilaiMhs = scn.nextInt();
+            totalNilaiMhs += parNilaiMhs;
+        }
+    return totalNilaiMhs;}
+
+    public double HitungRataRataNilaiMhs(double total, int jmlhMhs) {
+        return total/jmlhMhs;
+    }
+    
+}
---
+++ PBOIF2-10116378-LATIHAN37-ProgramRataRataNilai/src/pboif2/pkg10116378/latihan37/programrataratanilai/ProgramRataRataNilai.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10116378.latihan37.programrataratanilai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class ProgramRataRataNilai {
+     public static void main(String[] args) {
+          Mahasiswa mah = new Mahasiswa();
+         Scanner scn = new Scanner(System.in);
+
+         int n;
+         System.out.print("Masukkan Banyaknya Mahasiswa : ");
+         n = scn.nextInt();
+
+         mah.HitungTotal(mah.nilaiMhs, n);
+         mah.HitungRataRataNilaiMhs(mah.totalNilaiMhs, n);
+
+        System.out.println("\nMaka, Rata-rata Nilainya adalah : "+mah.HitungRataRataNilaiMhs(mah.totalNilaiMhs,n));
+        System.out.println("Developed by : Sri Utami Ningsih");
+    }
+}
